0f8af8e6fe0ff2ae86b475c4bded495028c99977
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2024-11-14 11:08:24 +0100

    Gjort Random för attack

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
         Weapon feinCannon = new Weapon();
         int AttackDamage = feinCannon.Attack();
         System.out.println(AttackDamage);
